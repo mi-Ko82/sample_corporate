8351bf0f2bbc0eae317c7aabf731641e271cbcaf
misao <mi3aaao159@gmail.com>
2021-09-21 23:59:08 +0900

    :sparkles: layout componentの追加

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,8 @@
-import Head from 'next/head'
-
-const siteName = '明るいHOUSE';
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>{siteName}</title>
-        <meta name="description" content={siteName + 'のホームページです'} />
-        <meta name="format-detection" content="telephone=no" />
-        <meta property="og:site_name" content={siteName} />
-        <meta property="og:url" content="" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={siteName} />
-        <meta property="og:description" content={siteName + 'のホームページです'} />
-        <meta property="og:image" content="" />
-        <meta property="og:locale" content="ja_JP" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-      </main>
-    </div>
+    <Layout title="トップページ">
+    </Layout>
   )
 }
